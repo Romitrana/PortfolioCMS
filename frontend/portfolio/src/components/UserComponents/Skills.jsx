8d37bcc186e0bcbd2b4747325f5cf79c1a91d54e
Romit Rana <romit2rana@gmail.com>
2025-12-17 19:20:55 +0530

    version 1.4.0 letter case issue resolved

--- frontend/portfolio/src/components/UserComponents/Skills.jsx
+++ frontend/portfolio/src/components/UserComponents/Skills.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
-import styles from "./Skills.module.css";
+import styles from "./skills.module.css";
 
 const tabs = ["Frontend", "Backend", "Language", "Tools"];
 
