ef602dd8277354e42c1b177e5bb46366a81ae487
Romit Rana <romit2rana@gmail.com>
2025-12-17 19:24:36 +0530

    version 1.4.0 letter case issue resolved

--- frontend/portfolio/src/pages/Certificates/NewCertificate.jsx
+++ frontend/portfolio/src/pages/Certificates/NewCertificate.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import styles from "../projects/NewProject.module.css";
+import styles from "../Projects/NewProject.module.css";
 
 export default function NewCertificate() {
   const navigate = useNavigate();
